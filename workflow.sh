b217930b2e161d340def42055dfd898012b217fd
Matt Norman <normanmr@ornl.gov>
2025-08-06 15:43:00 -0400

    Update workflow.sh

--- workflow.sh
+++ workflow.sh
@@ -1,6 +1,6 @@
-ssh -Y andes.olcf.ornl.gov
+> ssh -Y andes.olcf.ornl.gov
 
-module load paraview/5.13.3-osmesa imagemagick
+[imn@andes-login8:/lustre/orion/stf006/scratch/imn/turbine_viz/blades/paraview] 8-) module load paraview/5.13.3-osmesa imagemagick
 
 [imn@andes-login8:/lustre/orion/stf006/scratch/imn/turbine_viz/blades/paraview] 8-) cat job_andes.sh 
 #!/bin/bash
@@ -15,6 +15,8 @@ module load paraview/5.13.3-osmesa
 cd /lustre/orion/stf006/scratch/imn/turbine_viz/blades/paraview
 srun -n 1 -c 32 --gpus-per-task=1 --gpu-bind=closest /sw/andes/paraview/5.13.3-osmesa/bin/pvbatch ./script.py
 
-sbatch job_andes.sh
+[imn@andes-login8:/lustre/orion/stf006/scratch/imn/turbine_viz/blades/paraview] 8-) sbatch job_andes.sh
 
-ffmpeg -framerate 30 -i blades_animation.%04d.png -c:v libx264 -crf 18 blades_video.mp4
+# Copy data to local machine
+
+> ffmpeg -framerate 30 -i blades_animation.%04d.png -c:v libx264 -crf 18 blades_video.mp4
