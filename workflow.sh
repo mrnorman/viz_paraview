e3d782d6a55ade1077425b4cbaeee8678128d02f
Matt Norman <normanmr@ornl.gov>
2025-08-07 12:49:55 -0400

    Update workflow.sh

--- workflow.sh
+++ workflow.sh
@@ -20,4 +20,6 @@ srun -n 1 -c 32 --gpus-per-task=1 --gpu-bind=closest /sw/andes/paraview/5.13.3-o
 # Copy png files to local machine, e.g., scp dtn.olcf.ornl.gov:/lustre/orion/stf006/scratch/imn/turbine_viz/disk/paraview/*.png .
 # Install ffmpeg on your machine, e.g., snap install ffmpeg
 
-> ffmpeg -framerate 30 -i blades_animation.%04d.png -c:v libx264 -crf 18 blades_video.mp4
+> ffmpeg -framerate 30 -i disk_animation.%04d.png -c:v libx264 -preset slow -c:a aac -crf 23  -movflags +faststart -pix_fmt yuv420p disk_video.mp4
+> ffmpeg -framerate 30 -i blades_animation.%04d.png -c:v libx264 -preset slow -c:a aac -crf 23  -movflags +faststart -pix_fmt yuv420p blades_video.mp4
+> ffmpeg -i ./blades_video.mp4 -i ./disk_video.mp4 -filter_complex "[0:v]crop=w=iw:h=ih*0.7:x=0:y=ih*0.2[v0]; [1:v]crop=w=iw:h=ih*0.7:x=0:y=ih*0.2[v1]; [v0][v1]vstack=inputs=2[out]" -map "[out]"  -c:v libx264 -preset slow -c:a aac -crf 23  -movflags +faststart -pix_fmt yuv420p combined.mp4
